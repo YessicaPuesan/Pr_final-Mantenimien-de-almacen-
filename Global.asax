27f5b30ceb2200e05a82d102bebee166067bf93d
YessicaPuesan <61301046+YessicaPuesan@users.noreply.github.com>
2020-04-20 00:47:01 -0400

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="proyectofinal_prueba_.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(proyectofinal_prueba_.Startup))]
+namespace proyectofinal_prueba_
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
